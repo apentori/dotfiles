99021c661c208d5c1d2aa6863287ad6455b6f20f
Alexis Pentori <alexis@status.im>
2024-09-25 13:19:37 +0200

    vim: add ctlr S keymap
    Signed-off-by: Alexis Pentori <alexis@status.im>

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,4 +1,3 @@
-
 vim.g.mapleader = ' '
 -----------------
 -- Normal mode --
@@ -18,6 +17,8 @@ vim.keymap.set('n', '<C-Down>', ':resize +2<CR>', opts)
 vim.keymap.set('n', '<C-Left>', ':vertical resize -2<CR>', opts)
 vim.keymap.set('n', '<C-Right>', ':vertical resize +2<CR>', opts)
 
+-- Save
+vim.keymap.set('n', '<C-S>',':update<CR>', opts)
 -----------------
 -- Visual mode --
 -----------------
